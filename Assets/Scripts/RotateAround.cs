8bb484174a5365b119f718aafab9c61a22becbc7
lab31 <lab.123@fatec.sp.gov.br>
2023-09-18 12:18:36 -0300

    start

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RotateAround : MonoBehaviour
+    {
+        public Transform target;
+        public float speed;
+
+        private void Start()
+        {
+            if (target != null) return;
+            target = gameObject.transform;
+            Debug.Log("Target não especificado, padronizando para o próprio objeto.");
+        }
+
+        private void Update()
+        {
+           transform.RotateAround(target.position,Vector3.up, speed * Time.deltaTime);
+        }
+    }
+}
